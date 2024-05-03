bf55b40239e5dfb5a4df772e4244f1cdb0d856a1
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2024-05-03 20:12:49 +0900

    [level 0] Title: 양꼬치, Time: 0.03 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.java
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.java
@@ -1,11 +1,5 @@
 class Solution {
     public int solution(int n, int k) {
-        int answer = 0;
-        int count=0;
-        count=n/10;
-        
-        
-        answer=12000*n+k*2000-2000*count;
-        return answer;
+        return 12000*n + ((k-n/10))*2000;
     }
 }
