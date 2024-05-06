780827f8a4bd0bd08ef34da8a2460cf834c96bb6
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2024-05-07 08:26:15 +0900

    [level 0] Title: 5명씩, Time: 0.02 ms, Memory: 80.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181886. 5명씩/5명씩.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String[] solution(String[] names) {
+        
+        
+        String[] answer = new String[names.length%5 == 0 ? names.length/5 : (names.length/5)+1];
+        
+        for(int i=0,j=0; i<names.length; i+=5){
+            answer[j++]=names[i];
+        }
+        
+        
+        
+        return answer;
+    }
+}
