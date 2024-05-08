86110bb2619fd48317f45e8539fa1d39331d9568
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2024-05-08 20:48:59 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 1.81 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+class Solution {
+    public int solution(int[] num_list) {
+        return IntStream.range(0, num_list.length)
+                        .filter(index -> num_list[index] < 0)
+                        .findFirst() 
+                        .orElse(-1); 
+    }
+}
