fa70806db92389826bb2df41502292e59efd8764
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2024-05-11 10:44:34 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 0.41 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
@@ -1,8 +1,5 @@
 class Solution {
     public String[] solution(String my_string) {
-        String[] answer = my_string.trim().split("\\s+");
-        
-        
-        return answer;
+        return my_string.trim().split("\\s+");
     }
 }
