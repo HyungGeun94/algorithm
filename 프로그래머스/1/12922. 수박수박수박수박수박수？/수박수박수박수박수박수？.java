aecfbfa27ab240d5fcc1b029c97203d915643eab
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2024-05-11 10:58:22 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.10 ms, Memory: 79.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String solution(int n) {
+        String answer = "";
+        
+        StringBuffer sb = new StringBuffer();
+        
+        for(int i=1; i<=n; i++){
+            
+            if(i%2==0){
+                sb.append("박");
+            }else{
+                sb.append("수");
+            }
+            
+            
+        }
+        
+        return sb.toString();
+    }
+}
