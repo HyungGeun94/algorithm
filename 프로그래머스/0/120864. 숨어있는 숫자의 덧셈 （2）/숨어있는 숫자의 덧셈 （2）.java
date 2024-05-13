3ca6440bd87fc842016e7fa1a284dde6b7f05353
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2024-05-13 14:02:09 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 10.22 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
@@ -1,25 +1,36 @@
 class Solution {
     public int solution(String my_string) {
         int answer = 0;
-        StringBuilder number = new StringBuilder();
-
-        for (int i = 0; i < my_string.length(); i++) {
-            char ch = my_string.charAt(i);
-            if (Character.isDigit(ch)) { // 숫자인 경우
-                number.append(ch);
-            } else { // 숫자가 아닌 경우
-                if (number.length() > 0) {
-                    answer += Integer.parseInt(number.toString());
-                    number.setLength(0); // 숫자를 담은 StringBuilder 초기화
+        String storage = "";
+        
+        for(int i=0; i<my_string.length() ; i++){
+            System.out.println(storage);
+            
+            if(storage.length()==4){
+                 answer += Integer.parseInt(storage);
+                storage="";
+            }
+            
+            if(my_string.charAt(i)>='0' && my_string.charAt(i)<='9'){
+                
+                storage= storage +( (my_string.charAt(i)-'0'));
+                
+                if (i == my_string.length() - 1) {
+                    answer += Integer.parseInt(storage);
                 }
             }
+            
+            else if(storage.length()!=0){
+                answer += Integer.parseInt(storage);
+                storage="";
+            }
+            
+     
+            
         }
-
-        // 마지막 숫자 처리
-        if (number.length() > 0) {
-            answer += Integer.parseInt(number.toString());
-        }
-
+        
+        
+        
         return answer;
     }
 }
