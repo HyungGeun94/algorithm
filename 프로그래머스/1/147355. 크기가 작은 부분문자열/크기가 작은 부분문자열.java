e7147b596f7c6b8b6c2295282c1f13ca4055fe82
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2024-05-19 10:47:43 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 0.11 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
@@ -1,18 +1,11 @@
 class Solution {
     public int solution(String t, String p) {
-        int count = 0;
-        int pLen = p.length();
-        long pValue = Long.parseLong(p);
-
-        for (int i = 0; i <= t.length() - pLen; i++) {
-            String subStr = t.substring(i, i + pLen); 
-            long subStrValue = Long.parseLong(subStr);
-
-            if (subStrValue <= pValue) {
-                count++;
+        int answer = 0;
+        for (int i = 0; i < t.length()-p.length()+1; i++){
+            if (Long.parseLong(t.substring(i,i+p.length())) <= Long.parseLong(p)){
+                answer++;
             }
         }
-
-        return count;
+        return answer;
     }
 }
