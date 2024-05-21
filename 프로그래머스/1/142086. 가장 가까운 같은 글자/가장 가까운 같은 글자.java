f7225bd6d513d8c3ff27d9ab9d2b29f8fd6ebc0e
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2024-05-21 09:24:31 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 12.68 ms, Memory: 86.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String s) {
+        int[] answer = new int[s.length()];
+        
+        Arrays.fill(answer,-1);
+        
+        for(int i=s.length()-1; i>=1; i--){
+            for(int j=i-1; j>=0; j-- ){
+                
+                if(s.charAt(i)==s.charAt(j)){
+                    answer[i]=i-j;
+                    break;
+                    
+                }
+                
+                
+            }
+            
+            
+            
+        }
+        
+            
+            
+            
+            
+        
+        
+        
+        
+        return answer;
+    }
+}
