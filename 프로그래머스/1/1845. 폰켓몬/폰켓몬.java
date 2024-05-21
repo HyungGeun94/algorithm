95224d1dae5d1f4d4232ffb13794279cdc0968c5
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2024-05-21 11:53:08 +0900

    [level 1] Title: 폰켓몬, Time: 2.66 ms, Memory: 82.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        
+        List<Integer> list = new ArrayList<>();
+        
+        for(int i=0; i<nums.length; i++){
+            
+            if(list.size()==(nums.length/2)){
+                break;
+            }
+            
+            if(!list.contains(nums[i])){
+                list.add(nums[i]);
+                answer++;
+            }
+            
+            
+            
+        }
+        
+        
+        return answer;
+    }
+}
