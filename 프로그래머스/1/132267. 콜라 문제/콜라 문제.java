7fbe927c1f8ec9f76743d648e3d81658d718eec4
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2024-05-22 08:01:56 +0900

    [level 1] Title: 콜라 문제, Time: 0.01 ms, Memory: 78 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(int a, int b, int n) {
+        int answer = 0;
+        
+        
+        while(n>=a){
+            
+            n = n-a;
+            answer+=b;
+            n = n+b;
+            
+            
+            
+            
+        }
+        
+        
+        return answer;
+    }
+}
