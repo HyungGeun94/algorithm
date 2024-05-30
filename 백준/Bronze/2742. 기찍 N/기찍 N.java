82049401d7a4c6b75c8e80e683a3e4d08197abbf
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2024-05-30 09:52:19 +0900

    [Bronze IV] Title: 기찍 N, Time: 1136 ms, Memory: 33480 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+
+public class Main{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        
+        int num = scanner.nextInt();
+        
+        for(int i=num; i>=1; i--){
+            
+            System.out.println(i);
+        }
+        
+        
+        
+    }
+}
