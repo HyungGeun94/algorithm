987f2019c280341e71917dc39633b1dad1d25328
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2024-06-09 19:23:43 +0900

    [Bronze IV] Title: 평균 점수, Time: 172 ms, Memory: 17640 KB -BaekjoonHub

---
+++ 백준/Bronze/10039. 평균 점수/평균 점수.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+
+public class Main{
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        int sum=0;
+        for (int i=0; i<5; i++){
+
+            int number = scanner.nextInt();
+
+            if(number<=40){
+                sum+=40;
+            }else {
+                sum += number;
+            }
+
+        }
+
+        System.out.println(sum/5);
+
+    }
+}
