4b0aee0e132f54a842fccfacd0799ff3e9cb1c10
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2024-06-15 23:22:43 +0900

    [Bronze V] Title: 웰컴, Time: 120 ms, Memory: 13836 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,16 @@
+import java.math.BigInteger;
+import java.util.*;
+
+
+public class Main{
+
+    public static void main(String[] args){
+
+        System.out.println(
+                ".  .   .\n" +
+                "|  | _ | _. _ ._ _  _\n" +
+                "|/\\|(/.|(_.(_)[ | )(/.");
+    }
+
+ 
+}
