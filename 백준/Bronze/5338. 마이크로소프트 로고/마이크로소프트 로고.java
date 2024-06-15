2ae6943aa06cac377a5b5cecb6125ed5a5d60ecb
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2024-06-15 23:04:50 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 116 ms, Memory: 13864 KB -BaekjoonHub

--- 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -5,12 +5,19 @@ public class Main{
 
     public static void main(String[] args){
 
-        System.out.println("       " +
-                "_.-;;-._\n" +
+        System.out.println(
+                "       _.-;;-._\n"+
                 "'-..-'|   ||   |\n" +
                 "'-..-'|_.-;;-._|\n" +
                 "'-..-'|   ||   |\n" +
                 "'-..-'|_.-''-._|");
+
+
+
+
+
+
+
 
     }
 }
