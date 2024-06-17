dc189cadd6a32e0cfb43fc6d3979e93c5d68db40
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2024-06-17 19:11:09 +0900

    [Bronze IV] Title: 베라의 패션, Time: 216 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int count = scanner.nextInt();
+
+
+        System.out.println(count * (count-1));
+
+
+
+    }
+
+}
+
