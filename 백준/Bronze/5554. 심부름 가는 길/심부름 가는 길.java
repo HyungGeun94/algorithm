cdb02a33d5eebfcb2107fa900b58ea3255898eb3
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2024-06-17 20:17:27 +0900

    [Bronze IV] Title: 심부름 가는 길, Time: 172 ms, Memory: 17620 KB -BaekjoonHub

---
+++ 백준/Bronze/5554. 심부름 가는 길/심부름 가는 길.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int sum=0;
+
+        for(int i=0; i<4 ; i++) {
+            sum += scanner.nextInt();
+        }
+
+        System.out.println(sum/60);
+        System.out.println(sum%60);
+
+
+
+    }
+
+}
