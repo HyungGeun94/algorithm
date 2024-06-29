aabe45ed5146b7929890fd9dc38b12b2d2695931
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2024-06-29 23:20:37 +0900

    [Bronze IV] Title: Hello Judge, Time: 188 ms, Memory: 18304 KB -BaekjoonHub

---
+++ 백준/Bronze/9316. Hello Judge/Hello Judge.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int count = scanner.nextInt();
+
+        for(int i=0; i<count; i++){
+
+            System.out.println("Hello World, Judge "+(i+1)+"!");
+
+
+
+        }
+
+
+    }
+}
