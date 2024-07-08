a534ecf9caa5eb4f3114cb0ca0b93cd1cb297617
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2024-07-09 08:25:04 +0900

    [level 0] Title: 자릿수 더하기, Time: 5.94 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -1,14 +1,15 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
+        
         String str = n+"";
         
-        
-        for(int i=0 ; i<str.length(); i++){
-            answer+= str.charAt(i)-'0';
+        for(int i=0; i<str.length(); i++){
+            
+            answer+=str.charAt(i)-'0';
+            
         }
         
-
         
         return answer;
     }
