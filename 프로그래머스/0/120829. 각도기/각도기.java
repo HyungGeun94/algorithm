f5ee41885cc6c0963264a3981b3fa278991eea17
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2024-07-09 08:24:47 +0900

    [level 0] Title: 각도기, Time: 0.02 ms, Memory: 85.1 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.java
+++ 프로그래머스/0/120829. 각도기/각도기.java
@@ -1,6 +1,7 @@
 class Solution {
     public int solution(int angle) {
         int answer = 4;
+        
         if(angle<90){
             answer=1;
         }else if(angle==90){
@@ -8,6 +9,7 @@ class Solution {
         }else if(angle<180){
             answer=3;
         }
+        
         return answer;
     }
 }
