f4311de2e29ca6cc265a0ef5adfb56d38cdcebf3
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2024-07-09 08:33:42 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.02 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -1,16 +1,19 @@
 class Solution {
     public int[] solution(int n) {
-        int[] answer = new int[(int)(Math.round((double)n/2))];
         
-        for(int i=1,j=0 ; i<=n; i++){
-            if(i%2!=0){
-                answer[j]=i;
-                j++;
-            }
+        int count = n%2==0 ? n/2 : n/2+1;
+        
+        int[] answer = new int[count];
+        
+        int index=0;
+        
+        for(int i=1; i<=n; i+=2){
+            
+            answer[index++]=i;
+            
+            
             
         }
-        
-        
         
         return answer;
     }
