ff89934a4d660c5468a6dbe99ab0bf5699245ae0
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2024-07-10 08:33:23 +0900

    [level 0] Title: 순서쌍의 개수, Time: 0.08 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.java
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.java
@@ -1,15 +1,17 @@
 class Solution {
     public int solution(int n) {
+        int answer = 0;
+        int sqrtN = (int) Math.sqrt(n);
         
-        int answer =0;
-        
-        for(int i=1 ; i<=n; i++){
-            if(n%i==0){
-                answer++;
+        for (int i = 1; i <= sqrtN; i++) {
+            if (n % i == 0) {
+                if (i == n / i) {
+                    answer += 1; 
+                } else {
+                    answer += 2;
+                }
             }
-            
         }
-        
         
         return answer;
     }
