6a7500e97afe22b14a8d78e5fed14db0c19526e1
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2024-07-11 08:34:51 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.03 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.java
+++ 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.java
@@ -1,10 +1,28 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public int[] solution(int n, int[] numlist) {
         
-        return Arrays.stream(numlist)
-        .filter(num -> num % n == 0)
-        .toArray();
+        List<Integer> list = new ArrayList<>();
+        
+        for(int i=0; i<numlist.length; i++){
+            
+            if(numlist[i]%n==0){
+                list.add(numlist[i]);
+            }
+            
+        }
+        
+        int[] answer = new int[list.size()];
+        
+        for(int i=0; i<list.size(); i++){
+            
+            answer[i]=list.get(i);
+            
+        }
+        
+        
+        
+        return answer;
     }
 }
