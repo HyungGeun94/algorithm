707467aa82615296265bc13c0564f26001e2cec0
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2024-07-11 08:37:28 +0900

    [level 0] Title: 정수 찾기, Time: 0.03 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/0/181840. 정수 찾기/정수 찾기.java
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.java
@@ -1,12 +1,16 @@
 class Solution {
     public int solution(int[] num_list, int n) {
-        int answer=0;
-        for(int i : num_list){
-            if(i==n){
+        int answer = 0;
+        
+        for(int i :num_list){
+            
+            if( i==n){
                 answer=1;
                 break;
             }
+            
         }
+        
         
         return answer;
     }
