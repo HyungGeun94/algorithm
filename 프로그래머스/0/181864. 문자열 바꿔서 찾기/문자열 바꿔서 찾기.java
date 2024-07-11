aa33c49ff58e87c650ed9bb1a9478c598f2333a5
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2024-07-12 08:42:49 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.04 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
@@ -1,20 +1,12 @@
 class Solution {
     public int solution(String myString, String pat) {
-        int answer = 0;
         
-        StringBuffer sb = new StringBuffer();
-        
-        for(int i=0; i<myString.length();i ++){
-            
-            if(myString.charAt(i)=='A'){
-                sb.append("B");
-            }else{
-                sb.append("A");
-            }
-            
-        }
+        myString = myString.replace("A","c");
+        myString = myString.replace("B","A");
+        myString = myString.replace("c","B");
         
         
-        return new String(sb).contains(pat) ? 1 : 0;
+        return myString.contains(pat) ? 1 : 0 ;
+        
     }
 }
