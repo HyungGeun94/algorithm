d2e1967f02432e9f4c2cbab20d03abb721ab5da2
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2024-07-13 08:21:18 +0900

    [level 0] Title: 할 일 목록, Time: 0.05 ms, Memory: 84.4 MB -BaekjoonHub

--- 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -1,10 +1,29 @@
-import java.util.stream.IntStream;
+import java.util.*;
 
 class Solution {
     public String[] solution(String[] todo_list, boolean[] finished) {
-        return IntStream.range(0, finished.length)
-                        .filter(i -> !finished[i]) 
-                        .mapToObj(i -> todo_list[i]) 
-                        .toArray(String[]::new); 
+        
+        List<String> list = new ArrayList<>();
+        
+        
+        for(int i=0; i<todo_list.length; i++){
+            
+            if(!finished[i]){
+                
+                list.add(todo_list[i]);
+                
+            }
+            
+        }
+        
+        String[] answer = new String[list.size()];
+        
+        for(int i=0; i<list.size(); i++){
+            
+            answer[i]=list.get(i);
+            
+        }
+        
+        return answer;
     }
 }
