2019a42e308269dcca95263277a7eb1f8c505afe
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2024-07-13 08:35:15 +0900

    [level 0] Title: n 번째 원소까지, Time: 0.03 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.java
+++ 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.java
@@ -1,16 +1,18 @@
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
     public int[] solution(int[] num_list, int n) {
-        List<Integer> list = new ArrayList<>();
+        int[] answer = new int[n];
         
-        for (int i = 0; i < n; i++) {
-            list.add(num_list[i]);
+        for(int i=0; i<n; i++){
+            
+            answer[i]=num_list[i];
+            
+            
+            
         }
         
-        return list.stream()
-                   .mapToInt(Integer::intValue)
-                   .toArray();
+        
+        
+        
+        return answer;
     }
 }
