f55aeb22ea04778e604d250a5b252d42f7e8c5a8
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2024-07-14 08:59:54 +0900

    [level 0] Title: 카운트 다운, Time: 0.03 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
@@ -1,17 +1,15 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.IntStream;
-
 class Solution {
-    public int[] solution(int start, int end_num) {
-        List<Integer> list = new ArrayList<>();
-        for (int i = end_num; i <=start ; i++) {
-            list.add(i);
+    public int[] solution(int start_num, int end_num) {
+        
+        
+        int[] array = new int[start_num-end_num+1];
+        
+        for(int i=start_num; i>=end_num; i--){
+            
+            array[start_num-i]=i;
+            
         }
-        
-        Collections.reverse(list);
-        
-        return list.stream().mapToInt(Integer::intValue).toArray();
+
+        return array;
     }
 }
