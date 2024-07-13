e205db316728c66e098b7582bd807eeb2b3732c5
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2024-07-14 08:55:14 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.03 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.java
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.java
@@ -1,11 +1,19 @@
-import java.util.Arrays;
-import java.util.stream.IntStream;
-
 class Solution {
     public int solution(int[] num_list) {
-        return IntStream.range(0, num_list.length)
-                        .filter(index -> num_list[index] < 0)
-                        .findFirst() 
-                        .orElse(-1); 
+        int answer =-1;
+        
+        
+        for(int i=0; i<num_list.length; i++){
+            
+            if(num_list[i]<0){
+                
+                answer=i;
+                break;
+            }
+            
+            
+        }
+        
+        return answer;
     }
 }
