81c06e73803313db33cc7ee5f2708de458d976a6
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2024-07-15 08:31:27 +0900

    [level 0] Title: 더 크게 합치기, Time: 11.03 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -1,8 +1,10 @@
 class Solution {
     public int solution(int a, int b) {
         int answer = 0;
-        String first = ""+a+b;
-        String last = ""+b+a;
-        return Integer.parseInt(first) > Integer.parseInt(last) ? Integer.parseInt(first)  :Integer.parseInt(last)  ;
+        
+        int order = Integer.parseInt(""+a+b);
+        int reverse = Integer.parseInt(""+b+a);
+        
+        return Math.max(order,reverse);
     }
 }
