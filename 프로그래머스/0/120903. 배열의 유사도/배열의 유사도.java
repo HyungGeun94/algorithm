3eee4dfe08391e0b121bff00a8590516e9dc1755
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2024-07-15 09:00:54 +0900

    [level 0] Title: 배열의 유사도, Time: 0.05 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.java
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.java
@@ -1,18 +1,24 @@
+import java.util.*;
+
 class Solution {
     public int solution(String[] s1, String[] s2) {
         int answer = 0;
         
-        for(int i=0; i<s1.length; i++){
-            for(int j=0; j<s2.length; j++){
-                if(s1[i].equals(s2[j])){
-                    answer++;
-                    break;
-                }
-                
-                
-            }
+        List<String> list = new ArrayList<>();
+        
+        for(String str : s1){
             
+            list.add(str);
         }
+        
+        for(String str : s2){
+            
+            if(list.contains(str)){
+                answer++;
+            }
+        }
+        
+        
         
         return answer;
     }
