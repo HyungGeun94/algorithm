103d130bea074df364a95334f4c3f0f00bd00ced
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2024-07-17 07:48:42 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.09 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.java
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.java
@@ -1,13 +1,23 @@
 class Solution {
     public int solution(int[] dot) {
-        int answer = 3;
-        if(dot[0]>0 &&dot[1]>0){
+        int answer = 0;
+        
+        int x= dot[0];
+        int y= dot[1];
+        
+        if(x<0 && y<0){
+            answer=3;
+        }
+        if(x>0 && y>0){
             answer=1;
-        }else if(dot[0]>0 && dot[1]<0){
-            answer=4;
-        }else if(dot[0]<0 && dot[1]>0){
+        }
+        if(x<0 && y>0){
             answer=2;
         }
+        if(x>0 && y<0){
+            answer=4;
+        }
+        
         
         return answer;
     }
