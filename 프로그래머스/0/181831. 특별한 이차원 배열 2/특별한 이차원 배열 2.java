ea2c6378b7252d8601b87b14632674ef004b98d6
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2024-07-18 08:33:59 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 0.22 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.java
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.java
@@ -3,15 +3,19 @@ class Solution {
         int answer = 1;
         
         for(int i=0; i<arr.length; i++){
-            for(int j=0; j<arr.length; j++){
+            for(int j=i; j<arr.length; j++){
                 
                 if(arr[i][j]!=arr[j][i]){
                     answer=0;
                     break;
                 }
                 
+                
             }
-        }        
+            
+        }
+        
+        
         
         return answer;
     }
