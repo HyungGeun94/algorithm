856066c7dbdda67b12da15d783c37c93c7d79fde
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2024-07-18 08:25:34 +0900

    [Bronze V] Title: A+B - 3, Time: 224 ms, Memory: 17888 KB -BaekjoonHub

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -1,25 +1,20 @@
 import java.util.Scanner;
- 
-public class Main {
- 
-	public static void main(String[] args) {
-        
-		Scanner in = new Scanner(System.in);
-        
-        int size = in.nextInt();
- 
-        for(int i=0; i< size ; i++){
-        
-		int A = in.nextInt();
-        
-		int B = in.nextInt();
-        
-        System.out.println(A+B);
+
+class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int number = scanner.nextInt();
+
+        for(int i=0; i<number; i++){
+
+            int A = scanner.nextInt();
+            int B = scanner.nextInt();
+            System.out.println(A+B);
+
         }
-        
-        
-        
- 
-		in.close();
-	}
+
+
+    }
 }
