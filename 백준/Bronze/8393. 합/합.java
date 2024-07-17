5a4a6b8e3be4a8107628f82ddaef2dc8c29a805e
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2024-07-18 08:21:58 +0900

    [Bronze V] Title: 합, Time: 208 ms, Memory: 17736 KB -BaekjoonHub

--- 백준/Bronze/8393. 합/합.java
+++ 백준/Bronze/8393. 합/합.java
@@ -1,20 +1,27 @@
 import java.util.Scanner;
- 
-public class Main {
- 
-	public static void main(String[] args) {
- 
-		Scanner in = new Scanner(System.in);
-		int A = in.nextInt();
-        
-        int sum=0; 
-        
-        for(int i=1; i<=A; i++){
+
+class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int number = scanner.nextInt();
+
+        int sum=0;
+
+        for(int i=1; i<=number; i++){
+
             sum+=i;
+
         }
-		
+
         System.out.println(sum);
- 
-		in.close();
-	}
+
+
+
+
+
+
+
+    }
 }
