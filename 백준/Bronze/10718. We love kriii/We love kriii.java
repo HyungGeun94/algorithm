ec9e588f7edaebb7ba4160c8f283d47fed6daecd
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2024-07-18 08:10:58 +0900

    [Bronze V] Title: We love kriii, Time: 112 ms, Memory: 13896 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+
+//        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("강한친구 대한육군");
+        System.out.println("강한친구 대한육군");
+
+
+
+    }
+}
