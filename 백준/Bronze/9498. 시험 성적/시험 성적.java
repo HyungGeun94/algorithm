3121f6c39f5198e52046ca04d07d9aadb2898ac0
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2024-07-18 08:04:07 +0900

    [Bronze V] Title: 시험 성적, Time: 208 ms, Memory: 17772 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.java
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -1,24 +1,25 @@
 import java.util.Scanner;
- 
-public class Main {
- 
-	public static void main(String[] args) {
- 
-		Scanner scanner = new Scanner(System.in);
-		int A = scanner.nextInt();
-        
-        if(A>=90){
+
+class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int score = scanner.nextInt();
+
+        if(score>=90){
             System.out.println("A");
-        }else if(A>=80){
+        }else if(score>=80){
             System.out.println("B");
-        }else if(A>=70){
+        }else if(score>=70){
             System.out.println("C");
-        }else if(A>=60){
+        }else if(score>=60){
             System.out.println("D");
         }else{
             System.out.println("F");
         }
-        
-		scanner.close();
-	}
+
+
+
+
+    }
 }
