c61479946a789222ed0930382b987ee86646ce77
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2024-07-19 08:07:58 +0900

    [Bronze V] Title: ??!, Time: 220 ms, Memory: 17748 KB -BaekjoonHub

--- 백준/Bronze/10926. ？？！/？？！.java
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -1,16 +1,16 @@
-import java.util.Scanner;
- 
-public class Main {
- 
-	public static void main(String[] args) {
- 
-		Scanner in = new Scanner(System.in);
-        String str = in.nextLine();
-        
-            System.out.println(str+"??!");
+import java.util.*;
 
-		
- 
-		in.close();
-	}
+class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        
+        String sentence = scanner.nextLine();
+
+
+        System.out.println(sentence+"??!");
+
+
+    }
+
 }
