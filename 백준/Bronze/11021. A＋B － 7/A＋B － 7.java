7965677532024b086b3df41cfc280e66e4e4eb0a
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2024-07-19 08:01:25 +0900

    [Bronze V] Title: A+B - 7, Time: 260 ms, Memory: 18664 KB -BaekjoonHub

--- 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -1,31 +1,21 @@
-import java.util.Scanner;
- 
-public class Main {
- 
-	public static void main(String[] args) {
-        
-		Scanner in = new Scanner(System.in);
-        
-        int size = in.nextInt();
-        
-        for(int i=1; i<=size; i++){
-            
-        int A = in.nextInt();
-        
-		int B = in.nextInt();
-            
-            System.out.println("Case #"+i+": "+(A+B));
-        
-            
-            
-            
-        }
-        
+import java.util.*;
 
-        
-        
-        
- 
-		in.close();
-	}
+class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int size = scanner.nextInt();
+
+        for(int i=1; i<=size; i++){
+
+            int A = scanner.nextInt();
+            int B = scanner.nextInt();
+
+            System.out.println("Case #"+i+": " +(A+B));
+
+        }
+
+    }
+
 }
