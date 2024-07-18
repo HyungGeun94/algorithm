a780979f8f68f2b1691ddc59c9255f6852ac0660
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2024-07-19 07:58:14 +0900

    [Bronze V] Title: N 찍기, Time: 1084 ms, Memory: 32260 KB -BaekjoonHub

--- 백준/Bronze/2741. N 찍기/N 찍기.java
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -1,19 +1,16 @@
-import java.util.Scanner;
+import java.util.*;
 
-public class Main {
- 
-	public static void main(String[] args) {
-        
+class Main {
+    public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
-        
-        int num = scanner.nextInt();
-        
-        for(int i=1; i<=num; i++){
-            
-            System.out.println(i);
-            
-        }
- 
 
-	}
+        int number = scanner.nextInt();
+
+        for(int i=1; i<=number; i++){
+            System.out.println(i);
+        }
+
+    }
+
 }
