20051e3a0ae37553221d735f67998f69d93497b5
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2024-07-19 07:56:43 +0900

    [Bronze V] Title: A+B - 5, Time: 228 ms, Memory: 17828 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -1,26 +1,23 @@
-import java.util.Scanner;
- 
-public class Main {
- 
-	public static void main(String[] args) {
- 
-		Scanner in = new Scanner(System.in);
-        
-        while(true){
-		int A = in.nextInt();
-		int B = in.nextInt();
-        
-            if(A+B==0)
-            break;
-        
-		System.out.println(A+B);
-        
-    
-        
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        while(true) {
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+
+            if(A==0){
+                break;
+            }
+
+            System.out.println(A+B);
+
         }
-        
-        
- 
-		in.close();
-	}
+
+    }
+
 }
