25b0178407ae1def5c451796636b9aebff47fbea
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2024-07-19 08:12:42 +0900

    [Bronze V] Title: 개수 세기, Time: 216 ms, Memory: 17884 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -1,36 +1,23 @@
-import java.util.Scanner;
- 
-public class Main {
- 
-	public static void main(String[] args) {
-        
-		Scanner in = new Scanner(System.in);
-        
-        int size = in.nextInt();
-        
-        int count = 0;
-        
-        
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int size = scanner.nextInt();
+
         int[] arr = new int[size];
- 
-        for(int i=0; i< arr.length ; i++){
-        
-		arr[i] = in.nextInt();        
-        
-       
+
+        for (int i = 0; i < size; i++) {
+            arr[i]=scanner.nextInt();
         }
-        
-        int num = in.nextInt();
-        
-        for(int i=0; i<arr.length ; i++){
-            if(arr[i]==num){
-                count++;
-            }
-        }
-        
-        System.out.println(count);
-        
- 
-		in.close();
-	}
+
+        int standard = scanner.nextInt();
+
+        System.out.println(Arrays.stream(arr).filter(i -> i == standard).count());
+
+
+    }
+
 }
