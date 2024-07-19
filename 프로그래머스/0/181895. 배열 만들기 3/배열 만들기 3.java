1775c89de54d21cb0bfb2dec7f950d2f9ee05a92
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2024-07-20 07:37:31 +0900

    [level 0] Title: 배열 만들기 3, Time: 1.53 ms, Memory: 112 MB -BaekjoonHub

--- 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.java
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.java
@@ -1,17 +1,23 @@
+import java.util.*;
+
+
 class Solution {
     public int[] solution(int[] arr, int[][] intervals) {
-        int[] answer = new int[intervals[0][1]-intervals[0][0]+1+intervals[1][1]-intervals[1][0]+1];
-        int j=0;
         
-        for(int i=intervals[0][0] ;i<=intervals[0][1]; i++){
-            answer[j++]=arr[i];
+        List<Integer> list = new ArrayList<>();
+        
+        int[] copyArr1 = Arrays.copyOfRange(arr,intervals[0][0],intervals[0][1]+1);
+        int[] copyArr2 = Arrays.copyOfRange(arr,intervals[1][0],intervals[1][1]+1);
+        
+        int[] answer = new int[copyArr1.length + copyArr2.length];
+        
+        for(int i=0; i<copyArr1.length; i++){
+            answer[i]=copyArr1[i];
         }
-        
-         for(int i=intervals[1][0] ;i<=intervals[1][1]; i++){
-            answer[j++]=arr[i];
+        for(int i=0; i<copyArr2.length; i++){
+            
+            answer[i+copyArr1.length]=copyArr2[i];
         }
-        
-        
         
         
         return answer;
