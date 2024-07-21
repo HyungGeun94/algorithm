b17a6af260f01bb4f5b5ec462543e216b9b7806b
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2024-07-22 07:19:36 +0900

    [level 0] Title: 가위 바위 보, Time: 0.10 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
@@ -1,11 +1,24 @@
 class Solution {
     public String solution(String rsp) {
-        String answer = "";
         
-        for(int i=0; i<rsp.length(); i++){
-            answer += rsp.charAt(i) == '2' ? '0' :  rsp.charAt(i) == '0' ? '5' : '2' ; 
+        StringBuffer sb = new StringBuffer();
+        
+        char[] chArr = rsp.toCharArray();
+        
+        for(int i=0; i<chArr.length; i++){
+            
+            if(chArr[i]=='2'){
+                sb.append(0);
+            }else if(chArr[i]=='0'){
+                sb.append(5);
+            }else{
+                sb.append(2);
+            }
+            
         }
         
-        return answer;
+        
+        
+        return sb.toString();
     }
 }
