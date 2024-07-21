1f6b5c6ea8cfc35ef615191ec30a27bbc74adf2c
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2024-07-21 09:17:00 +0900

    [level 0] Title: 수 조작하기 2, Time: 8.92 ms, Memory: 88.5 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -1,23 +1,25 @@
 class Solution {
     public String solution(int[] numLog) {
-        StringBuffer sb=new StringBuffer();
         
+        StringBuffer sb = new StringBuffer();
         
         for(int i=1; i<numLog.length; i++){
-            if(numLog[i]-numLog[i-1]==1){
-                sb.append("w");
-            }else if(numLog[i]-numLog[i-1]==-1){
-                sb.append("s");
-            }else if(numLog[i]-numLog[i-1]==10){
-                sb.append("d");
-            }else if(numLog[i]-numLog[i-1]==-10){
-                sb.append("a");
+            int number = numLog[i]-numLog[i-1];
+            
+            if(number==1){
+                sb.append('w');
+            }else if(number==-1){
+                sb.append('s');
+            }else if(number==10){
+                sb.append('d');
+            }else{
+                sb.append('a');
             }
             
             
         }
         
-        
+
         return sb.toString();
     }
 }
