cc596cfe5eacd2c30df83f8748d6d086aa86e6c4
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2024-07-22 07:57:43 +0900

    [level 1] Title: 하샤드 수, Time: 10.52 ms, Memory: 80.4 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -1,13 +1,22 @@
 class Solution {
     public boolean solution(int x) {
+        boolean answer = false;
         
-        String str = (x+"");
-        int sum =0;
+        String str = ""+x;
+        
+        int sum=0;
         
         for(int i=0; i<str.length(); i++){
-            sum += str.charAt(i)-'0';
+            
+            sum+=(str.charAt(i)-'0');
+            
         }
         
-        return x%sum==0 ? true : false;
+        if(x%sum==0){
+            answer=true;
+        }
+        
+        
+        return answer;
     }
 }
