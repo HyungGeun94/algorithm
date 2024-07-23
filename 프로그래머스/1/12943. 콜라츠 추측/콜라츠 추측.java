2deb8bd7a6e5c8e531820747b6c770c81a4cc571
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2024-07-24 08:28:58 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.06 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,28 +1,31 @@
 class Solution {
     public int solution(int num) {
-        
-        long lNum = num;
         int answer = 0;
         
-        while(lNum!=1){
-            
-            if(lNum%2==0){
-                lNum/=2;
-                answer++;
-
-            }else{
-                lNum= (lNum *3) +1;
-                answer++;
-            }
-            
-            
-            if(answer>500){
-                break;
-            }
+        long number = num;
         
+        
+        while(true){
+            
+            if(number==1){
+                break;
+            }else if(answer>500){
+                answer=-1;
+                break;
+            }else{
+                if(number%2==0){
+                    number= number/2;
+                    answer++;
+                }else{
+                    number= (number*3+1);
+                    answer++;
+                }
+            }
+            
             
         }
         
-        return answer>500 ? -1 : answer;
+        
+        return answer;
     }
 }
