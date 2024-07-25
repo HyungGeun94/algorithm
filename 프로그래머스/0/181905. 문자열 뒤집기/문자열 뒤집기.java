9d3890f53ebd102dd81dcbe21e106df3fc1066e0
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2024-07-26 08:22:24 +0900

    [level 0] Title: 문자열 뒤집기, Time: 15.36 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,24 +1,9 @@
 class Solution {
     public String solution(String my_string, int s, int e) {
         
-        StringBuffer sb = new StringBuffer();
-        StringBuffer sb2 = new StringBuffer();
-        
-        sb.append(my_string.substring(0,s));
-        
-        sb2.append(my_string.substring(s,e+1));
-        
-        sb2.reverse();
+        StringBuffer sb = new StringBuffer(my_string.substring(s,e+1));
         
         
-        sb.append(sb2);
-        
-        sb.append(my_string.substring(e+1));
-        
-        
-        
-        
-        
-        return sb.toString();
+        return my_string.substring(0,s)+sb.reverse().toString()+my_string.substring(e+1);
     }
 }
