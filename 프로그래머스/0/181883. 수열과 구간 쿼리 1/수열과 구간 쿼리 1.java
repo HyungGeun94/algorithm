6cd632c05aa3bcef17d7a2e10981f7ec0d700800
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2024-07-27 07:51:14 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 4.96 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.java
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.java
@@ -1,16 +1,25 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] arr, int[][] queries) {
         
+        int[] answer = Arrays.copyOf(arr,arr.length);
+        
         for(int i=0; i<queries.length; i++){
+            
+            for(int j=0; j<answer.length; j++){
                 
-                for(int j=queries[i][0]; j<queries[i][1]+1; j++){
+                
+                if(queries[i][0]<=j && queries[i][1]>=j){
                     
-                    arr[j]+=1;
-                    
+                    answer[j]+=1;
                 }
+                
+            }
+                
+                
         }
         
-        
-        return arr;
+        return answer;
     }
 }
