5223f44aff430832722dd36a535a9dd6151651dd
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2024-07-27 07:40:35 +0900

    [level 0] Title: 배열 만들기 5, Time: 6.69 ms, Memory: 87.2 MB -BaekjoonHub

--- 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.java
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.java
@@ -1,6 +1,5 @@
 import java.util.*;
 
-
 class Solution {
     public int[] solution(String[] intStrs, int k, int s, int l) {
         
@@ -8,20 +7,25 @@ class Solution {
         
         for(int i=0; i<intStrs.length; i++){
             
-            if (Integer.parseInt(intStrs[i].substring(s,s+l))>k){
-                list.add(Integer.parseInt(intStrs[i].substring(s,s+l)));
+            int num = Integer.parseInt(intStrs[i].substring(s,s+l));
+            
+            if(num>k){
+                
+                list.add(num);
                 
             }
             
         }
         
+        
+        
         int[] answer = new int[list.size()];
         
-        for(int i=0; i<list.size(); i++){
+        for(int i=0; i<answer.length; i++){
+            
             answer[i]=list.get(i);
             
         }
-        
         
         return answer;
     }
