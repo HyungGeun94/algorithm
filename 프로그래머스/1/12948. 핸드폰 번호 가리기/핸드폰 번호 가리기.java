5f311b6f0c06c7880c644fbb1455cf42776a12a6
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2024-07-27 08:10:48 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 1.90 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -1,5 +1,8 @@
 class Solution {
     public String solution(String phone_number) {
-        return "*".repeat(phone_number.length()-4) + phone_number.substring(phone_number.length()-4,phone_number.length());
+        
+        
+        return phone_number.replaceAll("[0-9]","*").substring(0,phone_number.length()-4)
+                +phone_number.substring(phone_number.length()-4);
     }
 }
