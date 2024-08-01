53d5c853830e62b865848955700edd93e331744e
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2024-08-02 07:39:51 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.04 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,24 +1,15 @@
 class Solution {
     public boolean solution(String s) {
-        boolean answer = false;
-        
-        if(s.length() == 6 || s.length()==4 ){
-            
-            for(int i=0; i<s.length(); i++){
-                
-                if(s.charAt(i)>='0' && s.charAt(i)<='9'){
-                    answer=true;
-                }else{
-                    answer=false;
-                    break;
-                }
-            }
-            
+        if (s.length() != 4 && s.length() != 6) {
+            return false;
         }
-            
-
         
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) < '0' || s.charAt(i) > '9') {
+                return false;
+            }
+        }
         
-        return answer;
+        return true;
     }
 }
