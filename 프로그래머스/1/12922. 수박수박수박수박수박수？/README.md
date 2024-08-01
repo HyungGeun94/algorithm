5463879fe3f359484b779a7c27c28d97a023f7c6
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2024-08-02 07:11:28 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 1.08 ms, Memory: 66.6 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/README.md
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/README.md
@@ -1,10 +1,10 @@
 # [level 1] 수박수박수박수박수박수? - 12922 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12922) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12922?language=java) 
 
 ### 성능 요약
 
-메모리: 79.4 MB, 시간: 0.10 ms
+메모리: 66.6 MB, 시간: 1.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 11일 10:58:20
+2024년 08월 02일 07:11:24
 
 ### 문제 설명
 
--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -1,17 +1,14 @@
 class Solution {
     public String solution(int n) {
-        String answer = "";
         
         StringBuffer sb = new StringBuffer();
         
-        for(int i=1; i<=n; i++){
+        for(int i=0; i<n; i++){
             
-            if(i%2==0){
-                sb.append("박");
-            }else{
+            if(i%2==0)
                 sb.append("수");
-            }
-            
+            else
+                sb.append("박");
             
         }
         
