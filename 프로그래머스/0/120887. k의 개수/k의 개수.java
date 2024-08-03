620a28f50ab12735b25490d99e72f1b5b20030eb
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2024-08-04 07:29:20 +0900

    [level 0] Title: k의 개수, Time: 39.26 ms, Memory: 89.6 MB -BaekjoonHub

--- 프로그래머스/0/120887. k의 개수/k의 개수.java
+++ 프로그래머스/0/120887. k의 개수/k의 개수.java
@@ -2,18 +2,20 @@ class Solution {
     public int solution(int i, int j, int k) {
         int answer = 0;
         
-        StringBuffer sb= new StringBuffer();
-        for(int t=i; t<=j; t++){
-            sb.append(t);
-        }
         
-        char[] c = sb.toString().toCharArray();
-        
-        for(char cha : c){
-            if(cha==k+'0'){
-                answer++;
+        for(int z=i; z<=j; z++){
+            
+            String str = z+"";
+            
+            for(int x=0; x<str.length(); x++){
+                
+                if(str.charAt(x)-'0'==k){
+                    answer++;
+                }
             }
+            
         }
+        
         
         
         return answer;
