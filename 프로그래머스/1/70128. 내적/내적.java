403db0142a5cd97b6a3333e7a2078aa54c5e9a4b
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2024-08-08 23:19:28 +0900

    [level 1] Title: 내적, Time: 0.06 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.java
+++ 프로그래머스/1/70128. 내적/내적.java
@@ -3,7 +3,9 @@ class Solution {
         int answer = 0;
         
         for(int i=0; i<a.length; i++){
+            
             answer+= (a[i]*b[i]);
+            
         }
         
         return answer;
