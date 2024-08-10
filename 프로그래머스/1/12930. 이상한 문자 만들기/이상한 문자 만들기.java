a8fe14931bd65325a3b5cdecd1c304044f3029a8
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2024-08-11 08:42:48 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.10 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -1,25 +1,23 @@
 class Solution {
     public String solution(String s) {
-        StringBuilder result = new StringBuilder();
-        
-        // 공백도 포함하여 분리하는 정규식 사용
-        String[] words = s.split("(?<=\\s)|(?=\\s)");
-        
-        for (String word : words) {
-            StringBuilder transformedWord = new StringBuilder();
-            
-            for (int j = 0; j < word.length(); j++) {
-                char c = word.charAt(j);
-                if (j % 2 == 0) {
-                    transformedWord.append(Character.toUpperCase(c));
+        StringBuilder sb = new StringBuilder();
+        int index = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (c == ' ') {
+                sb.append(' ');
+                index = 0; 
+            } else {
+                if (index % 2 == 0) {
+                    sb.append(Character.toUpperCase(c));
                 } else {
-                    transformedWord.append(Character.toLowerCase(c));
+                    sb.append(Character.toLowerCase(c));
                 }
+                index++;
             }
-            
-            result.append(transformedWord);
         }
-        
-        return result.toString();
+        return sb.toString();
     }
 }
