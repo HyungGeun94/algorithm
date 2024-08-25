6abd48e95a7109548cb9f6e0f8c4598f9df5111a
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2024-08-25 23:36:10 +0900

    [level 0] Title: 접미사 배열, Time: 1.39 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
@@ -1,29 +1,20 @@
 import java.util.*;
 
-
 class Solution {
     public String[] solution(String my_string) {
         String[] answer = new String[my_string.length()];
         
         StringBuffer sb = new StringBuffer();
-        int idx=0;
         
         for(int i=my_string.length()-1; i>=0; i--){
-            sb.insert(0,my_string.charAt(i));
             
-            answer[idx++]=sb.toString();
+            sb.insert(0,my_string.charAt(i));
+            answer[my_string.length()-1-i]= sb.toString();
             
         }
         
+        
         Arrays.sort(answer);
-        
-        
-        
-        
-        
-        
-        
-        
         
         return answer;
     }
