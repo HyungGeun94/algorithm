a0e00305a5e671ff8f904980c461aa66f09cc1d4
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2024-08-29 21:20:39 +0900

    [level 0] Title: 문자열이 몇 번 등장하는지 세기, Time: 14.46 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.java
+++ 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.java
@@ -3,14 +3,18 @@ class Solution {
         int answer = 0;
         
         
-        
-            for(int i=0;i<myString.length()-pat.length()+1; i++){
-                
-            answer += myString.substring(i,i+pat.length()).contains(pat) ? 1 : 0;
-                
-                
+        for(int i=0; i<myString.length()-pat.length()+1; i++){
+            
+            String str =myString.substring(i,i+pat.length());
+            
+            System.out.println(str);
+            if(str.contains(pat)){
+                answer++;
             }
             
+            
+        }
+
             
         
         return answer;
