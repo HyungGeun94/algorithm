a94f2c511b67216b3e7fc72e267307a550989715
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2024-09-03 23:15:01 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 7.36 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -1,5 +1,11 @@
 class Solution {
     public String solution(String my_string, String overwrite_string, int s) {
-        return my_string.substring(0,s)+overwrite_string+(my_string.substring(s+overwrite_string.length()));
+        String answer = "";
+        
+        answer+=my_string.substring(0,s);
+        answer+=overwrite_string;
+        answer+=my_string.substring(s+overwrite_string.length());
+        
+        return answer;
     }
 }
