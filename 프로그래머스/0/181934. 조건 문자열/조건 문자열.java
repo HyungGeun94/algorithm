03313b26143f924e37a317f0a26c23a2a861a163
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2024-09-06 14:27:15 +0900

    [level 0] Title: 조건 문자열, Time: 0.04 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/0/181934. 조건 문자열/조건 문자열.java
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.java
@@ -2,20 +2,26 @@ class Solution {
     public int solution(String ineq, String eq, int n, int m) {
         int answer = 0;
         
-        if(ineq.equals(">") && eq.equals("=")){
+        if(ineq.equals(">")){
+            if(eq.equals("=")){
+                answer= n >= m ? 1 : 0;
+            }else{
+                answer= n > m ? 1 : 0;
+            }
             
-            answer = n >= m ? 1 : 0;
-        }else if(ineq.equals(">") && eq.equals("!")){
-            answer = n > m ? 1 : 0;
+        }else{
+            if(eq.equals("=")){
+                answer= n <= m ? 1 : 0;
+
+            }else{
+                answer= n < m ? 1 : 0;
+
+            }
 
             
         }
-        else if(ineq.equals("<") && eq.equals("=")){
-            answer = n <= m ? 1 : 0;
-        }
-        else{
-            answer = n < m ? 1 : 0;
-        }
+        
+        
         return answer;
     }
 }
