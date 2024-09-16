eb6cf4e83fa0de19d68fcfa42bfe63e4c05ece79
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2024-09-16 18:42:24 +0900

    [level 0] Title: 소인수분해, Time: 3.16 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/0/120852. 소인수분해/소인수분해.java
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.java
@@ -1,39 +1,21 @@
 import java.util.*;
 
-
 class Solution {
     public int[] solution(int n) {
         
-        int i=2;
+        List<Integer> array = new ArrayList<>();
         
-        List<Integer> list = new ArrayList<>();
-        
-        while(n!=1){
+        for (int i = 2; i <= n; i++) {
             
-                if(n%i==0){
-                    n/=i;
-                    list.add(i);
-                }else{
-                    i++;
+            while (n % i == 0) {
+                if (!array.contains(i)) {
+                    array.add(i);  
                 }
-            
+                n /= i;  
+            }
         }
         
-        Set<Integer> set = new HashSet<>(list);
-        list = new ArrayList<>(set);
-
-        
-        
-        
-        
-        int[] answer = new int[list.size()];
-        
-        for(int j=0; j<list.size();j++){
-            answer[j]=list.get(j);
-        }
-        
-        Arrays.sort(answer);
-        
-        return answer;
+       
+        return array.stream().mapToInt(i -> i).toArray();
     }
 }
