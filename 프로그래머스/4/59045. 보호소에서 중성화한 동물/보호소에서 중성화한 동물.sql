1a3afed38baeb7818d488f472ec78d2f9880fc08
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2024-12-06 10:38:58 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -0,0 +1,10 @@
+-- 코드를 입력하세요
+SELECT
+i.ANIMAL_ID,i.ANIMAL_TYPE,i.NAME
+FROM
+ANIMAL_INS as i
+JOIN
+ANIMAL_OUTS as o
+ON
+i.ANIMAL_ID= o.ANIMAL_ID
+where i.SEX_UPON_INTAKE like '%Intact%' and o.SEX_UPON_OUTCOME not like '%Intact%';
