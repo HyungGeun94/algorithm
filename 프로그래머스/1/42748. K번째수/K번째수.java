2bde613698ad6ac4402e89e00659151e09034478
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2025-01-12 09:45:29 +0900

    [level 1] Title: K번째수, Time: 0.44 ms, Memory: 84.6 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -2,19 +2,21 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        
         int[] answer = new int[commands.length];
         
+        int[] arr = null;
         
         for(int i=0; i<commands.length; i++){
-        
-            int[] arr = Arrays.copyOfRange(array,commands[i][0]-1,commands[i][1]);
             
-            Arrays.sort(arr);
-            
-            answer[i]=arr[commands[i][2]-1];
+                arr = Arrays.copyOfRange(array,commands[i][0]-1,commands[i][1]);
+                
+                Arrays.sort(arr);
+                
+                answer[i]=arr[commands[i][2]-1];
+                
             
         }
+        
         
         
         return answer;
