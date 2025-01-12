6571a29f7ff0c8d3d738a8045bc57052a749e8ce
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2025-01-12 09:11:29 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 28.01 ms, Memory: 107 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,42 +1,30 @@
-import java.util.*;
-
-
 class Solution {
     public String solution(int[] food) {
-        
-        List<Integer> list = new ArrayList<>();
-        
+        String answer = "";
         
         for(int i=1; i<food.length; i++){
             
-            if(food[i]>=2){
-                for(int j=0; j<food[i]/2 ; j++){
-                list.add(i);
-                }
+            food[i]=food[i]/2;
+            
+        }
+        
+        for(int i=1; i<food.length; i++){
+            
+            for(int j=0; j<food[i]; j++){
+                answer+=i;
+                
             }
             
         }
-        list.add(0);
+        answer+=0;
+        
         for(int i=food.length-1; i>=1; i--){
             
-            if(food[i]>=2){
-                for(int j=0; j<food[i]/2 ; j++){
-                list.add(i);
-                }
-            }
-            
+            for(int j=0; j<food[i]; j++){
+                answer+=i;
+                
+            }            
         }
-        
-        
-        String answer = "";
-        
-        
-        for(int i=0; i<list.size();i++){
-            
-            answer+=list.get(i);
-            
-        }
-        
         
         
         return answer;
