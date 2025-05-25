251f996bfb04aa748ce025979dcaf5db9abac32c
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2025-05-25 09:11:30 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.03 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -1,8 +1,5 @@
 class Solution {
     public int solution(int num1, int num2) {
-        
-        boolean isValid = (0<num1 && num1<=100) && (0<num2 && num2<=100) ;
-        
-        return isValid ? (int)(((double)num1/num2)*1000) : -1 ;
+        return (int)(((double)num1 / num2) * 1000) ;
     }
 }
