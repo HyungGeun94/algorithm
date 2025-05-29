fb1fef9f8a3e71e153a33e2981a73f9155e5d5c0
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2025-05-30 08:41:56 +0900

    [level 0] Title: n보다 커질 때까지 더하기, Time: 0.02 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.java
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.java
@@ -2,24 +2,16 @@ class Solution {
     public int solution(int[] numbers, int n) {
         int answer = 0;
         
-        
-        
         for(int i : numbers){
             
             answer+=i;
-            
+
             if(answer>n){
-                
-        
                 break;
             }
             
             
-            
         }
-        
-        
-        
         return answer;
     }
 }
