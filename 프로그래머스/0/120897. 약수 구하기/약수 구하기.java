01f545295e71ce9b78065584901b2afe2a2adf37
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2025-05-30 08:01:17 +0900

    [level 0] Title: 약수 구하기, Time: 4.61 ms, Memory: 91.5 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -1,9 +1,8 @@
-import java.util.stream.*;
+import java.util.Arrays;
+import java.util.stream.IntStream;
 
 class Solution {
     public int[] solution(int n) {
-        
-    
-        return IntStream.rangeClosed(1, n).filter(i -> n % i == 0).toArray();
+        return IntStream.rangeClosed(1, n).filter(num -> n % num == 0).toArray();
     }
 }
