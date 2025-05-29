26c4aef68041b85b081392f42a4d72bdc74cd3e8
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2025-05-29 11:07:17 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.53 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -6,8 +6,8 @@ class Solution {
         Arrays.sort(numbers);
         
         
+        return numbers[0]*numbers[1] >= numbers[numbers.length-2] * numbers[numbers.length-1] ? 
+                numbers[0]*numbers[1] : numbers[numbers.length-2] * numbers[numbers.length-1];
         
-        
-        return Math.max(numbers[0]*numbers[1],numbers[numbers.length-1]*numbers[numbers.length-2]);
     }
 }
