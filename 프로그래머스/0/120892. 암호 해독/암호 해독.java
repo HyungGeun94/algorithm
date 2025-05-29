66389cf13b581cd60d5e056362a36eb4ff947439
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2025-05-29 11:14:08 +0900

    [level 0] Title: 암호 해독, Time: 0.17 ms, Memory: 88.7 MB -BaekjoonHub

--- 프로그래머스/0/120892. 암호 해독/암호 해독.java
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.java
@@ -1,15 +1,15 @@
 class Solution {
     public String solution(String cipher, int code) {
-        String answer = "";
+        StringBuilder sb = new StringBuilder();
+        
         
         for(int i=code-1; i<cipher.length(); i+=code){
             
-            answer+=cipher.charAt(i);
-            
+            sb.append(cipher.charAt(i));
             
         }
         
         
-        return answer;
+        return sb.toString();
     }
 }
