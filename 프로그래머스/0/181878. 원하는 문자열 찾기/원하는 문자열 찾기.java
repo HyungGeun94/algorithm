2bbdc137b87227cc44f959df89f38c40d46dfbff
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2025-05-30 08:14:42 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 6.26 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.java
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.java
@@ -1,18 +1,9 @@
 class Solution {
     public int solution(String myString, String pat) {
-        int answer = 0;
         
         myString = myString.toLowerCase();
         pat = pat.toLowerCase();
         
-        if(myString.contains(pat)){
-            
-            answer=1;
-        }
-        
-        
-        
-        
-        return answer;
+        return myString.contains(pat) ? 1 : 0 ;
     }
 }
