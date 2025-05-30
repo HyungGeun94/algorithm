01f9af3c6e451164e6acf42d4cae1431d01eb59d
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2025-05-31 08:29:07 +0900

    [level 0] Title: 카운트 다운, Time: 7.38 ms, Memory: 85.3 MB -BaekjoonHub

--- 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
@@ -1,15 +1,10 @@
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+
+
 class Solution {
     public int[] solution(int start_num, int end_num) {
-        
-        
-        int[] array = new int[start_num-end_num+1];
-        
-        for(int i=start_num; i>=end_num; i--){
-            
-            array[start_num-i]=i;
-            
-        }
-
-        return array;
+        return IntStream.rangeClosed(end_num,start_num).boxed().sorted((a,b) -> Integer.compare(b,a)).mapToInt(i -> i ).toArray();
     }
 }
