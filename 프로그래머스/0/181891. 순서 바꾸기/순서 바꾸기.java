32d27d015a79c630e351b1a5d200e1fa3c2dbc86
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2025-05-31 08:05:59 +0900

    [level 0] Title: 순서 바꾸기, Time: 2.64 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
@@ -1,25 +1,20 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] num_list, int n) {
-        int[] answer = new int[num_list.length];
+        
+        List<Integer> list = new ArrayList<>();
         
         
         for(int i=n; i<num_list.length; i++){
-            
-            
-            answer[i-n]=num_list[i];
-            
+            list.add(num_list[i]);
         }
-        
-        int location = (num_list.length-n);
         
         for(int i=0; i<n; i++){
-            
-            
-            answer[location++]=num_list[i];
+            list.add(num_list[i]);
         }
         
         
-        
-        return answer;
+        return list.stream().mapToInt(num -> num).toArray();
     }
 }
