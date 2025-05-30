4f0832d01f3f02d3ff1f931af71534aa5669af4c
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2025-05-31 08:43:18 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 50.28 ms, Memory: 97.7 MB -BaekjoonHub

--- 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
+++ 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
@@ -1,13 +1,7 @@
+import java.util.*;
+
 class Solution {
     public int solution(String number) {
-        int answer = 0;
-        
-        for(int i=0; i<number.length(); i++){
-        
-            answer+=(number.charAt(i)-'0');
-        
-        }
-        
-        return answer%9;
+        return (Arrays.stream(number.split("")).mapToInt(Integer::parseInt).sum())%9 ;
     }
 }
