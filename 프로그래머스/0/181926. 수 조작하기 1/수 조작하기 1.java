6a5262f3f561b846c1325c73dd3a62426f4b80aa
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2025-05-31 08:45:20 +0900

    [level 0] Title: 수 조작하기 1, Time: 6.34 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
@@ -1,24 +1,11 @@
 class Solution {
     public int solution(int n, String control) {
-        int answer = n;
-        
-        for(int i=0; i<control.length(); i++){
-            
-            char ch = control.charAt(i);
-            
-            if(ch=='w'){
-                answer+=1;
-            }else if(ch=='s'){
-                answer-=1;
-                
-            }else if(ch=='d'){
-                answer+=10;
-            }else{
-                answer-=10;
-            }
+        for (char c : control.toCharArray()) {
+            if (c == 'w') n += 1;
+            else if (c == 's') n -= 1;
+            else if (c == 'd') n += 10;
+            else if (c == 'a') n -= 10;
         }
-        
-        
-        return answer;
+        return n;
     }
 }
