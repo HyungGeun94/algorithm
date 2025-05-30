4294f7e0a22363df0b8ae92e2172bdebc7c6ef45
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2025-05-31 08:31:24 +0900

    [level 0] Title: 배열 만들기 1, Time: 10.16 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
@@ -1,9 +1,8 @@
-import java.util.stream.*;
+import java.util.stream.IntStream;
 
 
 class Solution {
     public int[] solution(int n, int k) {
-  
-        return IntStream.rangeClosed(1, n).filter(i -> i % k == 0).toArray();
+        return IntStream.rangeClosed(1,n).filter(i -> i%k==0).toArray();
     }
 }
