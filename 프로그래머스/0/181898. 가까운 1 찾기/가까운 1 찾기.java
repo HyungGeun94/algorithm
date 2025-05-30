b034e354cbebeab069809cdcd7005b6e192ec801
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2025-05-31 08:13:43 +0900

    [level 0] Title: 가까운 1 찾기, Time: 2.55 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.java
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.java
@@ -1,21 +1,8 @@
+import java.util.stream.IntStream;
+
+
 class Solution {
     public int solution(int[] arr, int idx) {
-        int answer = -1;
-        
-        for(int i=idx; i<arr.length; i++){
-            
-            
-            if(arr[i]==1){
-                answer=i;
-                break;
-            }
-            
-            
-            
-        }
-        
-        
-        
-        return answer;
+        return IntStream.range(idx, arr.length).filter(i -> arr[i] == 1).findFirst().orElse(-1);
     }
 }
