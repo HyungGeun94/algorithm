9397d205f62ac41c365849c87a0958c254c255aa
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2025-06-01 12:14:20 +0900

    [level 0] Title: 부분 문자열, Time: 0.03 ms, Memory: 85.3 MB -BaekjoonHub

--- 프로그래머스/0/181842. 부분 문자열/부분 문자열.java
+++ 프로그래머스/0/181842. 부분 문자열/부분 문자열.java
@@ -1,11 +1,5 @@
 class Solution {
     public int solution(String str1, String str2) {
-        int answer = 0;
-        
-        if(str2.contains(str1)){
-            answer=1;
-        }
-        
-        return answer;
+        return str2.contains(str1) ? 1 : 0 ;
     }
 }
