cde7726c8a714fce9992490ad92bd08cbb9d4876
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2025-06-03 15:20:30 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.02 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -1,27 +1,22 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] num_list) {
-        
         int[] answer = new int[num_list.length+1];
         
-        
-        int last = num_list[num_list.length-1];
-        int before = num_list[num_list.length-2];
-        
-        for(int i=0; i< num_list.length; i++){
-            
+        for(int i=0; i<num_list.length; i++){
             answer[i]=num_list[i];
         }
         
-        if(last > before){
+        int lastNum = num_list[num_list.length-2];
+        int lastlastNum = num_list[num_list.length-1];
+        
+        if(lastlastNum > lastNum ){
             
-            answer[answer.length-1]=last-before;
+            answer[answer.length-1] = lastlastNum-lastNum;
             
-            
-        }else if(last <= before){
-            
-            answer[answer.length-1]=last*2;
-
-            
+        }else{
+            answer[answer.length-1] = lastlastNum *2;
         }
         
         
