83772ce3fab4690c127c433538401a60cd50a208
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2025-06-03 15:25:27 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.03 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
@@ -1,15 +1,15 @@
 class Solution {
     public int solution(int a, int b, int c) {
-        int answer = 0;
-        
-        if ( a==b && b==c){
-            answer=(a+b+c)*(a*a+b*b+c*c)*(a*a*a+b*b*b+c*c*c);
-        }else if(a !=b && b!=c && c!=a){
-            answer=a+b+c;
-        }else{
-            answer=(a+b+c)*(a*a+b*b+c*c);
-            }
-        
-        return answer;
+        int sum1 = a + b + c;
+        int sum2 = a * a + b * b + c * c;
+        int sum3 = a * a * a + b * b * b + c * c * c;
+
+        if (a == b && b == c) {
+            return sum1 * sum2 * sum3;
+        } else if (a == b || b == c || a == c) {
+            return sum1 * sum2;
+        } else {
+            return sum1;
+        }
     }
 }
