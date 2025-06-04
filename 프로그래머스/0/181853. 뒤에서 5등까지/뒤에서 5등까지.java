681b8a7fff0a8cb6fc10795be9be570fb982fe5d
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2025-06-04 22:50:53 +0900

    [level 0] Title: 뒤에서 5등까지, Time: 20.43 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.java
+++ 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.java
@@ -1,19 +1,13 @@
+import java.util.stream.IntStream;
 import java.util.*;
 
 class Solution {
     public int[] solution(int[] num_list) {
-        int[] answer = new int[5];
         
         Arrays.sort(num_list);
         
-        for(int i=0; i<=4; i++){
-            
-            answer[i]=num_list[i];
-            
-        }
         
         
-        
-        return answer;
+        return IntStream.range(0,5).map(i->num_list[i]).sorted().toArray();
     }
 }
