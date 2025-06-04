614453003caac0ea92c4da977a1b15e6cb62c459
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2025-06-04 22:46:21 +0900

    [level 0] Title: 꼬리 문자열, Time: 6.51 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.java
+++ 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.java
@@ -1,17 +1,9 @@
+import java.util.Arrays;
+
 class Solution {
     public String solution(String[] str_list, String ex) {
-        
-        StringBuffer sb = new StringBuffer();
-        
-        for(int i=0; i<str_list.length; i++){
-            
-            if(!str_list[i].contains(ex)){
-                
-                sb.append(str_list[i]);
-            }
-            
-        }
-        
-        return sb.toString();
+        return Arrays.stream(str_list)
+                      .filter(str -> !str.contains(ex))
+                      .reduce("", (a, b) -> a + b);
     }
 }
