30a5769c090795a6c370cd1fb81213f4f0126894
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2025-06-05 08:34:56 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 1.65 ms, Memory: 70 MB -BaekjoonHub

--- 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -1,19 +1,9 @@
+import java.util.Arrays;
+
 class Solution {
     public int solution(int[] num_list) {
-        int answer = 0;
-        
-        int sum=0;
-        int multi=1;
-        
-        
-        for(int i : num_list){
-            
-            sum+=i;
-            multi*=i;
-            
-        }
-        
-        
-        return sum*sum>multi ? 1 : 0;
+        int sum = Arrays.stream(num_list).sum();
+        int product = Arrays.stream(num_list).reduce(1, (a, b) -> a * b);
+        return product < sum * sum ? 1 : 0;
     }
 }
