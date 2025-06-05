2407c9a289ac51a4d2725a2ffbd4de6313226f5f
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2025-06-05 11:18:36 +0900

    [level 0] Title: 배열 비교하기, Time: 1.09 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.java
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.java
@@ -1,27 +1,22 @@
+import java.util.stream.IntStream;
+import java.util.*;
+
 class Solution {
     public int solution(int[] arr1, int[] arr2) {
+        int answer = 0;
         
-        int length1 = arr1.length;
+        int size1 = arr1.length;
+        int size2 = arr2.length;
         
-        int length2 = arr2.length;
-        
-        
-        if(length1!=length2){
-            
-            return length1 > length2 ? 1 : length1==length2 ? 0 : -1;
-            
+        if(size1 != size2){
+            answer = size2 > size1 ? -1 : 1;
             
         }else{
-            int sum1 = 0;
-            int sum2 = 0;
             
-            for(int i=0; i<length1; i++){
-                sum1+=arr1[i];
-                sum2+=arr2[i];
-
-            }
-
-            return sum1 > sum2 ? 1 : sum1 == sum2 ? 0 : -1;
+            int sum1 = Arrays.stream(arr1).sum();
+            int sum2 = Arrays.stream(arr2).sum();
+            
+            answer= sum1 == sum2 ? 0 : sum1>sum2 ? 1 : -1;
             
             
             
@@ -29,6 +24,6 @@ class Solution {
         
         
         
-        
+        return answer;
     }
 }
