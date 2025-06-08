d6e727ab808461d8ae45448785f06542ee7cfe0a
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2025-06-08 13:50:23 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 7.23 ms, Memory: 93.4 MB -BaekjoonHub

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
@@ -1,24 +1,15 @@
-import java.util.*;
+import java.util.stream.Collectors;
 
 class Solution {
     public long solution(long n) {
-
-        String str = n+"";
-
-        Character[] charArray = new Character[str.length()];
-
-        for(int i = 0; i < str.length(); i++) {
-            charArray[i] = str.charAt(i);
-        }
-
-        Arrays.sort(charArray,Collections.reverseOrder());
-
-        StringBuffer sb = new StringBuffer();
         
-        for(char c : charArray) {
-            sb.append(c);
-        }
-        
-        return Long.parseLong(sb.toString());
+     
+        return Long.parseLong(String.valueOf(n)                   
+          .chars()                         
+          .mapToObj(c -> (char)c)          
+          .sorted((a, b) -> b - a)       
+          .map(ch -> String.valueOf(ch))           
+          .collect(Collectors.joining())  
+        );
     }
 }
