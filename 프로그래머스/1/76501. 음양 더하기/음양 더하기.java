f2e7bb24134b8fa461df3ec95643cd3dd23c4fd0
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2025-06-09 08:48:38 +0900

    [level 1] Title: 음양 더하기, Time: 0.07 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -2,18 +2,16 @@ class Solution {
     public int solution(int[] absolutes, boolean[] signs) {
         int answer = 0;
         
-        for(int i=0; i<signs.length; i++){
-            
+        for(int i=0; i<absolutes.length; i++){
             
             if(signs[i]){
-                answer+=absolutes[i];   
+                answer+=absolutes[i];
             }else{
                 answer-=absolutes[i];
             }
             
             
         }
-        
         
         return answer;
     }
