3e972c1e688ea568f81bf3a83f5ff8bfd02a80c7
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2025-06-10 21:31:07 +0900

    [level 0] Title: 날짜 비교하기, Time: 19.63 ms, Memory: 90.8 MB -BaekjoonHub

--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.java
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.java
@@ -1,13 +1,9 @@
 class Solution {
     public int solution(int[] date1, int[] date2) {
         
-        int date11 = Integer.parseInt(""+date1[0]+date1[1]+date1[2]);
-        int date22 = Integer.parseInt(""+date2[0]+date2[1]+date2[2]);
+        String str1 = date1[0]+""+date1[1]+date1[2];
+        String str2 = date2[0]+""+date2[1]+date2[2];
         
-        
-        
-            
-        
-        return date11< date22 ? 1 : 0;
+        return Integer.parseInt(str1) < Integer.parseInt(str2) ? 1:0;
     }
 }
