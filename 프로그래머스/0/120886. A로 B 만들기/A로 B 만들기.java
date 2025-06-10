396289c9bf16df205b2fa9d94c7fcdbf987e8b6d
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2025-06-10 21:48:51 +0900

    [level 0] Title: A로 B 만들기, Time: 1.22 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.java
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.java
@@ -2,21 +2,11 @@ import java.util.*;
 
 class Solution {
     public int solution(String before, String after) {
-        int answer = 0;
-        
-        
         char[] chArr1 = before.toCharArray();
         char[] chArr2 = after.toCharArray();
         
         Arrays.sort(chArr1);
         Arrays.sort(chArr2);
-        
-        if(Arrays.equals(chArr1,chArr2)){
-            
-            answer=1;
-        }
-            
-        
-        return answer;
+        return Arrays.equals(chArr1,chArr2) ? 1: 0;
     }
 }
