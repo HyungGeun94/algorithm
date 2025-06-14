b8f1c808a4b185d7a35320609fc0cbd2e26361ab
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2025-06-14 10:07:59 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 7.13 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.java
+++ 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.java
@@ -1,22 +1,12 @@
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.*;
+import java.util.stream.*;
 
 class Solution {
     public String solution(String s) {
-        
-        Character[] charArray = new Character[s.length()];
-        
-        for (int i = 0; i < s.length(); i++) {
-            charArray[i] = s.charAt(i);
-        }
-        
-        Arrays.sort(charArray, Collections.reverseOrder());
-        
-        StringBuilder sortedString = new StringBuilder();
-        for (char c : charArray) {
-            sortedString.append(c);
-        }
-        
-        return sortedString.toString();
+        return s.chars()
+                .mapToObj(c -> (char) c)
+                .sorted(Comparator.reverseOrder())
+                .map(String::valueOf)
+                .collect(Collectors.joining());
     }
 }
