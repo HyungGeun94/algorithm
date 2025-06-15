5542d87b9b876328635795b18cadda7342b0965a
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2025-06-15 09:28:57 +0900

    [level 0] Title: 코드 처리하기, Time: 7.08 ms, Memory: 90.5 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -1,29 +1,30 @@
 class Solution {
     public String solution(String code) {
         
+        StringBuilder sb = new StringBuilder();
+        
         int mode = 0;
         
-        StringBuffer sb = new StringBuffer();
-        
-        
-        for(int i=0; i<code.length(); i++ ){
-            if(code.charAt(i)=='1'){
-                mode = mode == 0 ? 1 : 0 ;
-            }else if(mode == 0 && i%2==0){
-                sb.append(code.charAt(i));
+        for(int i=0; i<code.length(); i++){
+            char ch = code.charAt(i);
+            
+            if(ch=='1' && mode == 0){
+                mode =1;
+                
+            }else if(ch=='1' && mode ==1){
+                mode=0;
+                
+            }else if(mode ==0 && i%2==0){
+                sb.append(ch);
+                
             }else if(mode == 1 && i%2!=0){
-                sb.append(code.charAt(i));
+                sb.append(ch);
+                
             }
+            
+            
         }
         
-        if(sb.length()==0){
-            sb.append("EMPTY");
-        }
-        
-        
- 
-        
-        
-        return sb.toString();
+        return sb.toString().isEmpty() ? "EMPTY" : sb.toString();
     }
 }
