50c36933870a1975969ae1c72107f965e7823112
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2025-06-23 23:14:06 +0900

    [level 1] Title: 삼총사, Time: 0.04 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.java
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -3,10 +3,7 @@ class Solution {
         int answer = 0;
         
         for(int i=0; i<number.length-2; i++){
-            
-            for(int j=i+1; j<number.length-1 ; j++){
-                
-                
+            for(int j=i+1; j<number.length-1; j++){
                 for(int k=j+1; k<number.length; k++){
                     
                     if(number[i]+number[j]+number[k]==0){
@@ -15,9 +12,7 @@ class Solution {
                     
                 }
             }
-            
         }
-        
         
         return answer;
     }
