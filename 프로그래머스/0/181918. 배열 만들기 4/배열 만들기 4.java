e0fd7724d71fefa6bad42f5b41be8da095c5f8e9
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2025-06-26 13:17:58 +0900

    [level 0] Title: 배열 만들기 4, Time: 23.38 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
@@ -2,14 +2,19 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] arr) {
+        
         List<Integer> stk = new ArrayList<>();
+        int i = 0;
 
-        for (int i = 0; i < arr.length; i++) {
-            if (stk.isEmpty() || stk.get(stk.size() - 1) < arr[i]) {
+        while (i < arr.length) {
+            if (stk.isEmpty()) {
                 stk.add(arr[i]);
+                i++;
+            } else if (stk.get(stk.size() - 1) < arr[i]) {
+                stk.add(arr[i]);
+                i++;
             } else {
                 stk.remove(stk.size() - 1);
-                i--; // 현재 인덱스를 다시 검사하기 위해 감소
             }
         }
 
