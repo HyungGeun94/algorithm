d859bef25ea5168fc29cc403608f9c3a5e4e9cf6
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2025-06-26 19:23:51 +0900

    [level 0] Title: 직사각형 넓이 구하기, Time: 0.34 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.java
+++ 프로그래머스/0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.java
@@ -2,23 +2,22 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] dots) {
+        int answer = 0;
         
-        Set<Integer> setX = new HashSet<>();
-        Set<Integer> setY = new HashSet<>();
+        int[] x = new int[dots.length];
+        int[] y = new int[dots.length];
+        
         
         for(int i=0; i<dots.length; i++){
-            for(int j=0; j<dots[i].length; j++){
-                
-                setX.add(dots[i][0]);
-                setY.add(dots[i][1]);
-                
-            }
+            
+            x[i]=dots[i][0];
+            y[i]=dots[i][1];            
+            
         }
         
-        List<Integer> listX = new ArrayList<>(setX);
-        List<Integer> listY = new ArrayList<>(setY);
+        Arrays.sort(x);
+        Arrays.sort(y);
         
-        return Math.abs(listX.get(0)-listX.get(1))*Math.abs(listY.get(0)-listY.get(1));
-        
+        return (x[x.length-1]-x[0]) * (y[y.length-1]-y[0]);
     }
 }
