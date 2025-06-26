37d0e88a9a2b03d3262fa69cf26bc6239ca22ec3
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2025-06-26 18:51:46 +0900

    [level 0] Title: 문자열 밀기, Time: 11.97 ms, Memory: 91.5 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
@@ -1,41 +1,11 @@
-import java.util.*;
-
-
 class Solution {
     public int solution(String A, String B) {
-        int answer = 0;
-        
-        if(A.equals(B)){
-            return answer;
+        String rotated = A;
+        for (int i = 0; i < A.length(); i++) {
+            if (rotated.equals(B)) return i;
+            rotated = rotated.charAt(rotated.length() - 1) + 
+                rotated.substring(0, rotated.length() - 1);
         }
-        
-        StringBuffer sbA = new StringBuffer(A);
-  
-        
-        
-        for(int i=0; i<A.length();i++){
-            
-            sbA.insert(0,A.charAt(A.length()-1));
-            sbA.deleteCharAt(A.length());
-            A=sbA.toString();
-            if(A.equals(B)){
-                answer=i+1;
-                break;
-            }else{
-                answer=-1;
-            }
-        }
-        
-
-        
-        
-        
-        
-        
-        
-            
-        
-        
-        return answer;
+        return -1;
     }
 }
