bfbe496c799279845b1e7c6c8f960c0fdfb49629
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2025-06-26 13:14:17 +0900

    [level 0] Title: 배열 만들기 6, Time: 49.08 ms, Memory: 157 MB -BaekjoonHub

--- 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.java
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.java
@@ -2,37 +2,26 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] arr) {
-        
+        int i=0;
         
         List<Integer> list = new ArrayList<>();
-        
-        int i=0;
         
         while(i<arr.length){
             
             if(list.isEmpty()){
                 list.add(arr[i]);
-            }else if(list.get(list.size()-1) == arr[i]){
+                i++;
+            }else if(list.get(list.size()-1)== arr[i]){
                 list.remove(list.size()-1);
-            }else{
+                i++;
+            }else if(list.get(list.size()-1)!= arr[i]){
                 list.add(arr[i]);
+                i++;
             }
-            
-            i++;
         }
         
-        int[] answer = new int[list.size()];
+        int[] answer = list.stream().mapToInt(integer->integer).toArray();
         
-        for(int j=0; j<list.size(); j++){
-            answer[j]=list.get(j);
-        }
-        
-        if(answer.length==0){
-            answer=new int[]{-1};
-        }
-        
-
-        
-        return answer;
+        return answer.length == 0 ? new int[]{-1} : answer;
     }
 }
