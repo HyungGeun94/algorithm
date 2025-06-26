df53642ca2bf3d19903375727d74ecba89d9ca24
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2025-06-26 18:36:18 +0900

    [level 0] Title: 다항식 더하기, Time: 13.04 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.java
+++ 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.java
@@ -1,44 +1,35 @@
+import java.util.*;
+
 class Solution {
     public String solution(String polynomial) {
+        
+        int xcontainInt =0 ;
+        int number = 0;
+        
+        String[] strArr = polynomial.split(" ");
+        
+        for(int i=0 ; i<strArr.length ; i++){
+            
+            if(strArr[i].contains("x")){
+                System.out.println("first" + strArr[i]);
 
-        int x = 0;
-        int y = 0;
-        String answer = "";
+                if(strArr[i].length()==1){
+                    xcontainInt+=1;
+                }else{
+                    strArr[i]= strArr[i].replace("x","");
+                     System.out.println(strArr[i]);
+                    xcontainInt+= Integer.parseInt(strArr[i]);
 
-        for(String str : polynomial.split(" ")) {
-
-            if(str.equals("+")) {
-                continue;
-            }
-
-            if(str.contains("x")) {
-
-                if(str.equals("x")) {
-                    x += 1;
-                } else {
-                    x += Integer.parseInt(str.replace("x", ""));   
                 }
-
-            } else {
-                y += Integer.parseInt(str);
-
+                
+                
+            }else if(!strArr[i].equals("+")){
+                number+= Integer.parseInt(strArr[i]);
             }
+            
         }
-
-        if (x > 0) {
-
-            answer = (x == 1 ? "x" : Integer.toString(x) + "x");
-
-            if (y > 0) {
-                answer = answer + " + ";
-            }
-        }
-
-        if (y > 0) {
-
-            answer = answer + Integer.toString(y);
-        }
-
-        return answer;
+        if (xcontainInt == 0) return number + "";
+        if (number == 0) return (xcontainInt == 1 ? "x" : xcontainInt + "x");
+        return (xcontainInt == 1 ? "x" : xcontainInt + "x") + " + " + number;
     }
 }
