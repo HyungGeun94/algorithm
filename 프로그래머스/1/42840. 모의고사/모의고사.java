1c6be6bcc0324b9a6772d135a0456bbb85c9b67a
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2025-06-26 21:45:29 +0900

    [level 1] Title: 모의고사, Time: 1.23 ms, Memory: 91.6 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,48 +1,50 @@
 import java.util.*;
 
-
 class Solution {
     public int[] solution(int[] answers) {
+        int[] answer = {};
         
-        int[] first =  {1, 2, 3, 4, 5};
-        int hitfirst = 0;
-        int[] second = {2, 1, 2, 3, 2, 4, 2, 5};
-        int hitsecond = 0;
-        int[] third =  {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-        int hitthird = 0;
+        int sum1 = 0;
+        int sum2 = 0;
+        int sum3 = 0;
+        int[] answer1 = {1,2,3,4,5};
+        int[] answer2 = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] answer3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
         
-        for(int i=0; i<answers.length; i++){
-            
-            
-            if(answers[i]==first[i%5]){
-                hitfirst++;
-            }
-            
-            if(answers[i]==second[i%8]){
-                hitsecond++;
-            }
-            
-            if(answers[i]==third[i%10]){
-                hitthird++;
-            }
-        }   
         
-        int maxScore = Math.max(hitfirst, Math.max(hitsecond, hitthird));
+            for(int i=0; i<answers.length; i++){
+                
+                if(answer1[i%answer1.length]==answers[i]){
+                    sum1++;
+                }
+                if(answer2[i%answer2.length]==answers[i]){
+                    sum2++;
+                }
+                if(answer3[i%answer3.length]==answers[i]){
+                    sum3++;
+                }
+                
+            }    
         
-        List<Integer> topScorers = new ArrayList<>();
-        if(hitfirst == maxScore) {
-            topScorers.add(1);
-        }
-        if(hitsecond == maxScore) {
-            topScorers.add(2);
-        }
-        if(hitthird == maxScore) {
-            topScorers.add(3);
-        }
+        System.out.println(sum1);
+        System.out.println(sum2);
+        System.out.println(sum3);
         
-        int[] answer = new int[topScorers.size()];
-        for(int i = 0; i < topScorers.size(); i++) {
-            answer[i] = topScorers.get(i);
+        
+        if(sum1==sum2 && sum2==sum3){
+            return new int[]{1,2,3,};
+        }else if(sum1> sum2 && sum1> sum3){
+            return new int[]{1};
+        }else if(sum2 > sum1 && sum2 > sum3){
+            return new int[]{2};
+        }else if(sum3 > sum1 && sum3 > sum2){
+            return new int[]{3};
+        }else if(sum1==sum2){
+            return new int[]{1,2};
+        }else if(sum2==sum3){
+            return new int[]{2,3};
+        }else if(sum3==sum1){
+            return new int[]{1,3};
         }
         
         return answer;
