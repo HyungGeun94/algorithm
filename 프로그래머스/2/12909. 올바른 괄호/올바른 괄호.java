809ab06b0ab03a0bd5536d745261b6b0177f4222
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2025-06-26 13:01:57 +0900

    [level 2] Title: 올바른 괄호, Time: 19.91 ms, Memory: 55.8 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -2,33 +2,27 @@ import java.util.*;
 
 class Solution {
     boolean solution(String s) {
+        boolean answer = true;
         
+        Stack<String> stack = new Stack<>();
         
-        Stack<Character> stack = new Stack<>();
-
-        for (int i = 0; i < s.length(); i++) {
+        for(int i=0; i<s.length(); i++){
             
-            char c = s.charAt(i);
+            char ch = s.charAt(i);
             
-            if (c == '(') {
-                stack.push(c);
-
-            }else if (c == ')') {
-                if (stack.isEmpty()) {
-                    return false;
-                }else{
-                    stack.pop();
-                }
+            if(ch == '('){
+                
+                stack.add("(");
+                
+            }else if( !stack.isEmpty() && stack.peek() == "("){
+                stack.pop();            
+            }else{
+                return false;
             }
-        }
-        
-        if(!stack.isEmpty()){
-            return false;
+            
         }
 
 
-        return true;
-        
-  
+        return stack.isEmpty() ? true : false;
     }
 }
