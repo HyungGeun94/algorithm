7859136ef68c48cc7748ae329e552011f976701e
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2025-06-27 18:10:51 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.02 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -1,22 +1,14 @@
 class Solution {
     public int solution(int[] common) {
-        int answer = 0;
         
-        if(common[1]- common[0] == common[2]-common[1]){
-            int x = common[1]- common[0];
+        if(common[1]-common[0]==common[2]-common[1]){
             
-            answer=common[common.length-1]+x;
-
-                
-            
+            return common[common.length-1]+common[1]-common[0];
         }else{
-            int x = common[1]/  common[0];
-
-            answer=common[common.length-1]*x;
-
+            
+            return common[common.length-1]*common[1]/common[0];
         }
         
         
-        return answer;
     }
 }
