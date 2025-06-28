92b625849bcaf0ea38f90e1e6b2a481c4b946fbe
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2025-06-28 13:09:03 +0900

    [level 2] Title: 최솟값 만들기, Time: 2.31 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -4,19 +4,19 @@ class Solution
 {
     public int solution(int []A, int []B)
     {
-        int answer = 0;
         
+        int sum= 0;
         Arrays.sort(A);
         Arrays.sort(B);
         
         for(int i=0; i<A.length; i++){
             
-            answer+=(A[i]*B[A.length-1-i]);
-            
+            sum+= (A[i] * B[A.length-1-i]);
             
         }
+        
+        
 
-
-        return answer;
+        return sum;
     }
 }
