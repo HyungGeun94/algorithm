1acd3f0e02e0d04e92f1d083a8caa8a98399565f
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2025-06-29 16:17:20 +0900

    [level 1] Title: 기사단원의 무기, Time: 373.47 ms, Memory: 112 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -1,26 +1,38 @@
-import java.util.*;
-
 class Solution {
     public int solution(int number, int limit, int power) {
         int answer = 0;
+        int yaksu =0;
         
-        for (int i = 1; i <= number; i++) {
-            int count = 0;
+        for(int i=1; i<=number; i++){
             
-            for (int j = 1; j * j <= i; j++) {
-                if (i % j == 0) {
-                    count++;
-                    if (j != i / j) {
-                        count++;
+            
+            
+            
+            for(int j=1; j<=(int)Math.sqrt(i); j++){
+
+                if(i%j==0){
+                    if(j*j==i){
+                     yaksu+=1;
+                    }else{
+                        yaksu+=2;
                     }
                 }
-                if (count > limit) {
-                    count = power;
-                    break;
-                }
+                
+                
             }
             
-            answer += count;
+            System.out.println(yaksu);
+            
+            
+            
+            if(yaksu>limit){
+                answer+=power;
+            }else{
+                answer+=yaksu;
+            }
+            
+            yaksu=0;
+            
         }
         
         return answer;
