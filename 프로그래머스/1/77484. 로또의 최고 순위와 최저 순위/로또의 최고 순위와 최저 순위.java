a3b03824fc7fe5a93d94650f49e79cf0c0186edb
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2025-06-30 13:08:54 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.47 ms, Memory: 88.1 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -1,41 +1,36 @@
 import java.util.*;
+import java.util.stream.IntStream;
 
 class Solution {
     public int[] solution(int[] lottos, int[] win_nums) {
         int[] answer = new int[2];
         
-        // 0의 개수와 일치하는 번호의 개수 계산
-        int zeroCount = 0;
-        int matchCount = 0;
+        Arrays.sort(lottos);
+        Arrays.sort(win_nums);
+        int matchCount =0;
+        int zeroCount = 0 ;
+        int[] rank = {6,6,5,4,3,2,1};
         
-        Set<Integer> winNumsSet = new HashSet<>();
-        for (int num : win_nums) {
-            winNumsSet.add(num);
-        }
-        
-        for (int num : lottos) {
-            if (num == 0) {
-                zeroCount++;
-            } else if (winNumsSet.contains(num)) {
-                matchCount++;
+        for(int i=0; i<lottos.length; i++){
+            for(int j=0; j<win_nums.length; j++){
+                
+                if(lottos[i]==0){
+                    zeroCount++;
+                    break;
+                }
+                
+                if(lottos[i]==win_nums[j]){
+                    matchCount++;
+                    break;
+                }
+                
+                
             }
         }
         
-        // 최고 순위와 최저 순위 계산
-        answer[0] = getRank(matchCount + zeroCount); // 최고 순위
-        answer[1] = getRank(matchCount); // 최저 순위
+            answer[0]=rank[matchCount+zeroCount];
+            answer[1]=rank[matchCount];
         
         return answer;
-    }
-    
-        private int getRank(int matchCount) {
-        switch (matchCount) {
-            case 6: return 1;
-            case 5: return 2;
-            case 4: return 3;
-            case 3: return 4;
-            case 2: return 5;
-            default: return 6;
-        }
     }
 }
