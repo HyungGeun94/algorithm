76d13351dc276cd3671793007a900c738070e7b2
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2025-07-05 12:02:00 +0900

    [level 2] Title: 숫자의 표현, Time: 1.62 ms, Memory: 54.1 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,29 +1,18 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        
-        for(int i=1; i<=n; i++){
-            
-            int sum=0;
-            
-            for(int j=i; j<=n; j++){
-                
-                sum+=j;
-                
-                if(sum==n){
+
+        for (int start = 1; start <= n; start++) {
+            int sum = 0;
+            for (int next = start; sum < n; next++) {
+                sum += next;
+                if (sum == n) {
                     answer++;
                     break;
                 }
-                
-                if(sum>n){
-                    break;
-                }
-                
             }
-            
-            
         }
-        
+
         return answer;
     }
 }
