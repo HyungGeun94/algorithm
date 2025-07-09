a2a852b53d537f681172a1786fd7dfe58186986b
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2025-07-09 11:28:26 +0900

    [level 2] Title: 롤케이크 자르기, Time: 269.52 ms, Memory: 195 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -1,35 +1,31 @@
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
-public class Solution {
+class Solution {
     public int solution(int[] topping) {
-        int n = topping.length;
-        int[] leftCount = new int[n];
-        int[] rightCount = new int[n];
-        
+        int answer = 0;
+
         Set<Integer> leftSet = new HashSet<>();
-        Set<Integer> rightSet = new HashSet<>();
-        
-        for (int i = 0; i < n; ++i) {
-            leftSet.add(topping[i]);
-            leftCount[i] = leftSet.size();
+        Map<Integer, Integer> rightMap = new HashMap<>();
+
+        for (int t : topping) {
+            rightMap.put(t, rightMap.getOrDefault(t, 0) + 1);
         }
-        
-        for (int i = n - 1; i >= 0; --i) {
-            rightSet.add(topping[i]);
-            rightCount[i] = rightSet.size();
-        }
-        
-        int fairCuts = 0;
-        
-        for (int i = 0; i < n - 1; ++i) {
-            if (leftCount[i] == rightCount[i + 1]) {
-                fairCuts++;
+
+        for (int i = 0; i < topping.length; i++) {
+            int current = topping[i];
+
+            leftSet.add(current);
+
+            rightMap.put(current, rightMap.get(current) - 1);
+            if (rightMap.get(current) == 0) {
+                rightMap.remove(current);
+            }
+
+            if (leftSet.size() == rightMap.size()) {
+                answer++;
             }
         }
-        
-        return fairCuts;
-    }
-    
 
+        return answer;
+    }
 }
