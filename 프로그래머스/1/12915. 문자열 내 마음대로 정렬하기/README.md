3a62e00766f59e4cddaa292556b2dcfa60d1e56d
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2025-08-07 23:41:28 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 2.14 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/README.md
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 문자열 내 마음대로 정렬하기 - 12915 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12915) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12915?language=java) 
 
 ### 성능 요약
 
-메모리: 87.5 MB, 시간: 0.81 ms
+메모리: 79.4 MB, 시간: 2.14 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 12일 09:24:05
+2025년 08월 07일 23:41:22
 
 ### 문제 설명
 
--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
@@ -1,14 +1,20 @@
 import java.util.*;
 
-class Solution {
+public class Solution {
     public String[] solution(String[] strings, int n) {
-        Arrays.sort(strings, (a, b) -> {
-            if (a.charAt(n) == b.charAt(n)) {
-                return a.compareTo(b);
-            } else {
-                return Character.compare(a.charAt(n), b.charAt(n));
+        Arrays.sort(strings, new Comparator<String>() {
+            public int compare(String s1, String s2) {
+                char c1 = s1.charAt(n);
+                char c2 = s2.charAt(n);
+
+                if (c1 == c2) {
+                    return s1.compareTo(s2); 
+                } else {
+                    return c1 - c2;
+                }
             }
         });
+
         return strings;
     }
 }
