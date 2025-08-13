368ed05c632f506d61f5944e9e5ce3eb38a568e8
전형근 <152036928+HyungGeun94@users.noreply.github.com>
2025-08-13 20:33:09 +0900

    [level 2] Title: 구명보트, Time: 13.18 ms, Memory: 58.3 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -1,22 +1,18 @@
-import java.util.*;
+import java.util.Arrays;
 
 class Solution {
     public int solution(int[] people, int limit) {
-        Arrays.sort(people); 
-        
-        int answer = 0;
-        
-        int left = 0; 
-        int right = people.length - 1;
-        
+        Arrays.sort(people);
+        int left = 0, right = people.length - 1;
+        int boats = 0;
+
         while (left <= right) {
-            if (people[left] + people[right] <= limit) { 
-                left++; 
+            if (people[left] + people[right] <= limit) {
+                left++;                     
             }
-            right--; 
-            answer++; 
+            right--;                          
+            boats++;
         }
-        
-        return answer;
+        return boats;
     }
 }
